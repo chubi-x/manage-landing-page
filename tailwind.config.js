e3064941c2b1ddf4d6c0a74715191555b56b46dc
chubi-x <chubixax@protonmail.com>
2022-08-01 12:28:30 +0100

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
         darkGrayishBlue: 'hsl(227,12%,61%)',
         veryPaleRed: 'hsl(13,100%,96%)',
         veryLightGray: 'hsl(0,0%,98%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)'
       }
     },
   },
